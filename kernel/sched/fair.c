9fc17da0c58c541755defb701d3b79e6b70ec339
Rick Yiu <rickyiu@google.com>
2020-10-15 15:41:52 +0300

    ANDROID: sched: Add vendor hooks for find_energy_efficient_cpu
    Add hooks for vendor specific find_energy_efficient_cpu logic.
    
    Bug: 170507310
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Change-Id: I064b501017e32d4f22f8128bed8bf3a1508ab15b
    (cherry picked from commit 2f108e2ec6e89609cbae32c5d13d6ad9f2e858cb)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7634,7 +7634,11 @@ static int find_energy_efficient_cpu(struct task_struct *p, int prev_cpu)
 	unsigned long prev_thermal_cap = 0;
 	struct sched_domain *sd;
 	struct perf_domain *pd;
-	struct energy_env eenv;
+	int new_cpu = INT_MAX;
+
+	trace_android_rvh_find_energy_efficient_cpu(p, prev_cpu, sync, &new_cpu);
+	if (new_cpu != INT_MAX)
+		return new_cpu;
 
 	rcu_read_lock();
 	pd = rcu_dereference(rd->pd);
