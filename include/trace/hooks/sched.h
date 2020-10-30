2c2427f9026284aefbafa379fe8639f5d3d161f3
Wei Wang <wvw@google.com>
2020-10-30 08:49:45 +0300

    Revert "Revert "ANDROID: schedutil: add vendor hook for adjusting util to freq calculation""
    This reverts commit 3fa75e00

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -247,6 +247,11 @@ DECLARE_RESTRICTED_HOOK(android_rvh_sched_exec,
 	TP_PROTO(bool *cond),
 	TP_ARGS(cond), 1);
 
+DECLARE_HOOK(android_vh_map_util_freq,
+	TP_PROTO(unsigned long util, unsigned long freq,
+		unsigned long cap, unsigned long *next_freq),
+	TP_ARGS(util, freq, cap, next_freq));
+
 DECLARE_HOOK(android_vh_em_cpu_energy,
 	TP_PROTO(struct em_perf_domain *pd,
 		unsigned long max_util, unsigned long sum_util,
--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -185,7 +185,11 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 
 	util = map_util_perf(util);
 	freq = map_util_freq(util, freq, max);
-	trace_sugov_next_freq_tp(policy->cpu, util, max, freq);
+	trace_android_vh_map_util_freq(util, freq, max, &next_freq);
+	if (next_freq)
+		freq = next_freq;
+	else
+		freq = map_util_freq(util, freq, max);
 
 	if (freq == sg_policy->cached_raw_freq && !sg_policy->need_freq_update)
 		return sg_policy->next_freq;
