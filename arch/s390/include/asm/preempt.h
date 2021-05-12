33001b4c1561c53d0b0acd4ae4723b66d8d11891
Valentin Schneider <valentin.schneider@arm.com>
2021-05-12 12:46:36 +0300

    sched/core: Initialize the idle task with preemption disabled
    As pointed out by commit
    
      de9b8f5dcbd9 ("sched: Fix crash trying to dequeue/enqueue the idle thread")
    
    init_idle() can and will be invoked more than once on the same idle
    task. At boot time, it is invoked for the boot CPU thread by
    sched_init(). Then smp_init() creates the threads for all the secondary
    CPUs and invokes init_idle() on them.
    
    As the hotplug machinery brings the secondaries to life, it will issue
    calls to idle_thread_get(), which itself invokes init_idle() yet again.
    In this case it's invoked twice more per secondary: at _cpu_up(), and at
    bringup_cpu().
    
    Given smp_init() already initializes the idle tasks for all *possible*
    CPUs, no further initialization should be required. Now, removing
    init_idle() from idle_thread_get() exposes some interesting expectations
    with regards to the idle task's preempt_count: the secondary startup always
    issues a preempt_disable(), requiring some reset of the preempt count to 0
    between hot-unplug and hotplug, which is currently served by
    idle_thread_get() -> idle_init().
    
    Given the idle task is supposed to have preemption disabled once and never
    see it re-enabled, it seems that what we actually want is to initialize its
    preempt_count to PREEMPT_DISABLED and leave it there. Do that, and remove
    init_idle() from idle_thread_get().
    
    Secondary startups were patched via coccinelle:
    
      @begone@
      @@
    
      -preempt_disable();
      ...
      cpu_startup_entry(CPUHP_AP_ONLINE_IDLE);
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Acked-by: Peter Zijlstra <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20210512094636.2958515-1-valentin.schneider@arm.com

--- arch/s390/include/asm/preempt.h
+++ arch/s390/include/asm/preempt.h
@@ -29,6 +29,12 @@ static inline void preempt_count_set(int pc)
 				  old, new) != old);
 }
 
+#define init_task_preempt_count(p)	do { } while (0)
+
+#define init_idle_preempt_count(p, cpu)	do { \
+	S390_lowcore.preempt_count = PREEMPT_DISABLED; \
+} while (0)
+
 static inline void set_preempt_need_resched(void)
 {
 	__atomic_and(~PREEMPT_NEED_RESCHED, &S390_lowcore.preempt_count);
@@ -46,17 +52,10 @@ static inline bool test_preempt_need_resched(void)
 
 static inline void __preempt_count_add(int val)
 {
-	/*
-	 * With some obscure config options and CONFIG_PROFILE_ALL_BRANCHES
-	 * enabled, gcc 12 fails to handle __builtin_constant_p().
-	 */
-	if (!IS_ENABLED(CONFIG_PROFILE_ALL_BRANCHES)) {
-		if (__builtin_constant_p(val) && (val >= -128) && (val <= 127)) {
-			__atomic_add_const(val, &S390_lowcore.preempt_count);
-			return;
-		}
-	}
-	__atomic_add(val, &S390_lowcore.preempt_count);
+	if (__builtin_constant_p(val) && (val >= -128) && (val <= 127))
+		__atomic_add_const(val, &S390_lowcore.preempt_count);
+	else
+		__atomic_add(val, &S390_lowcore.preempt_count);
 }
 
 static inline void __preempt_count_sub(int val)
@@ -88,6 +87,12 @@ static inline void preempt_count_set(int pc)
 {
 	S390_lowcore.preempt_count = pc;
 }
+
+#define init_task_preempt_count(p)	do { } while (0)
+
+#define init_idle_preempt_count(p, cpu)	do { \
+	S390_lowcore.preempt_count = PREEMPT_DISABLED; \
+} while (0)
 
 static inline void set_preempt_need_resched(void)
 {
@@ -125,14 +130,10 @@ static inline bool should_resched(int preempt_offset)
 
 #endif /* CONFIG_HAVE_MARCH_Z196_FEATURES */
 
-#define init_task_preempt_count(p)	do { } while (0)
-/* Deferred to CPU bringup time */
-#define init_idle_preempt_count(p, cpu)	do { } while (0)
-
 #ifdef CONFIG_PREEMPTION
-extern asmlinkage void preempt_schedule(void);
+extern void preempt_schedule(void);
 #define __preempt_schedule() preempt_schedule()
-extern asmlinkage void preempt_schedule_notrace(void);
+extern void preempt_schedule_notrace(void);
 #define __preempt_schedule_notrace() preempt_schedule_notrace()
 #endif /* CONFIG_PREEMPTION */
 
