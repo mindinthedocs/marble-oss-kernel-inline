25cfdcf2ae9d79e0f86c8cde7f04d4ee9db6cb6c
Will Deacon <will@kernel.org>
2021-07-30 12:24:32 +0100

    sched: Reject CPU affinity changes based on task_cpu_possible_mask()
    Reject explicit requests to change the affinity mask of a task via
    set_cpus_allowed_ptr() if the requested mask is not a subset of the
    mask returned by task_cpu_possible_mask(). This ensures that the
    'cpus_mask' for a given task cannot contain CPUs which are incapable of
    executing it, except in cases where the affinity is forced.
    
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <Valentin.Schneider@arm.com>
    Reviewed-by: Quentin Perret <qperret@google.com>
    Link: https://lore.kernel.org/r/20210730112443.23245-6-will@kernel.org

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2782,13 +2782,15 @@ static int __set_cpus_allowed_ptr_locked(struct task_struct *p,
 					 struct rq *rq,
 					 struct rq_flags *rf)
 {
+	const struct cpumask *cpu_allowed_mask = task_cpu_possible_mask(p);
 	const struct cpumask *cpu_valid_mask = cpu_active_mask;
+	bool kthread = p->flags & PF_KTHREAD;
 	unsigned int dest_cpu;
 	int ret = 0;
 
 	update_rq_clock(rq);
 
-	if (p->flags & PF_KTHREAD || is_migration_disabled(p)) {
+	if (kthread || is_migration_disabled(p)) {
 		/*
 		 * Kernel threads are allowed on online && !active CPUs.
 		 *
@@ -2798,6 +2800,11 @@ static int __set_cpus_allowed_ptr_locked(struct task_struct *p,
 		 * set_cpus_allowed_common() and actually reset p->cpus_ptr.
 		 */
 		cpu_valid_mask = cpu_online_mask;
+	}
+
+	if (!kthread && !cpumask_subset(new_mask, cpu_allowed_mask)) {
+		ret = -EINVAL;
+		goto out;
 	}
 
 	/*
