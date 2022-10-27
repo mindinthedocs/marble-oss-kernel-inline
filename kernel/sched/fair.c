cb65f26caab68c49b03b65c3b14988ae3c84993f
Sai Harshini Nimmala <quic_snimmala@quicinc.com>
2022-10-28 01:07:29 +0300

    ANDROID: Sched: Add vendor hooks required for WALT
    Adding vendor hooks in several areas of the scheduler code.
    These are needed for implementing Qualcomm's load tracking scheme WALT
    via vendor modules. WALT was originally submitted upstream, but due to
    complexities it was decided that a better path forward would to be
    improve on existing PELT. Link to upstream submission of WALT can be
    found below.
    
    This reverts commit 7e2c96035407 ("Revert "ANDROID: Sched: Add
    restricted vendor hooks for scheduler""). This change effectively brings
    back the original change.
    
    Bug: 200103201
    Bug: 243793188
    Link: https://lore.kernel.org/lkml/1477638642-17428-2-git-send-email-markivx@codeaurora.org/
    Signed-off-by: Ashay Jaiswal <quic_ashayj@quicinc.com>
    Signed-off-by: Sai Harshini Nimmala <quic_snimmala@quicinc.com>
    Change-Id: I03327dc00d35e6fabcf8b5e254c4da548943d1a7

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4793,7 +4793,10 @@ static inline int task_fits_cpu(struct task_struct *p, int cpu)
 
 static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 {
-	if (!sched_asym_cpucap_active())
+	bool need_update = true;
+
+	trace_android_rvh_update_misfit_status(p, rq, &need_update);
+	if (!sched_asym_cpucap_active() || !need_update)
 		return;
 
 	if (!p || p->nr_cpus_allowed == 1) {
@@ -4955,6 +4958,7 @@ place_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	 */
 	se->deadline = se->vruntime + vslice;
 	trace_android_rvh_place_entity(cfs_rq, se, initial, vruntime);
+	trace_android_rvh_place_entity(cfs_rq, se, initial, &vruntime);
 }
 
 static void check_enqueue_throttle(struct cfs_rq *cfs_rq);
@@ -9261,6 +9265,7 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	if (!capacity)
 		capacity = 1;
 
+	trace_android_rvh_update_cpu_capacity(cpu, &capacity);
 	cpu_rq(cpu)->cpu_capacity = capacity;
 	trace_sched_cpu_capacity_tp(cpu_rq(cpu));
 
