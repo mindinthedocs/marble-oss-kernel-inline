78b336204b3a0bc564d6478f9b17da59a2d1fa55
xieliujie <xieliujie@oppo.com>
2023-04-28 11:38:02 +0300

    ANDROID: sched: Add vendor hooks to compute new cpu freq.
    add vendor hooks to compute new cpu freq for oem feature.
    
    Bug: 280021175
    
    Signed-off-by: xieliujie <xieliujie@oppo.com>
    Change-Id: I2b8e1f76f3b9792148f153190b862face679ebbd

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -532,3 +532,4 @@ EXPORT_SYMBOL_GPL(GKI_struct_swap_slots_cache);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_set_sugov_sched_attr);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_iowait);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_set_sugov_update);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_map_util_freq_new);
