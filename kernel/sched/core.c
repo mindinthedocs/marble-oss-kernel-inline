c0bd7c9af289e98fca272233306c6107875a6bb3
Shrikanth Hegde <sshegde@linux.ibm.com>
2024-02-16 11:44:33 +0530

    sched/core: Simplify code by removing duplicate #ifdefs
    There's a few cases of nested #ifdefs in the scheduler code
    that can be simplified:
    
      #ifdef DEFINE_A
      ...code block...
        #ifdef DEFINE_A       <-- This is a duplicate.
        ...code block...
        #endif
      #else
        #ifndef DEFINE_A     <-- This is also duplicate.
        ...code block...
        #endif
      #endif
    
    More details about the script and methods used to find these code
    patterns can be found at:
    
      https://lore.kernel.org/all/20240118080326.13137-1-sshegde@linux.ibm.com/
    
    No change in functionality intended.
    
    [ mingo: Clarified the changelog. ]
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lore.kernel.org/r/20240216061433.535522-1-sshegde@linux.ibm.com

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1730,7 +1730,6 @@ static void cpu_util_update_eff(struct cgroup_subsys_state *css);
 #endif
 
 #ifdef CONFIG_SYSCTL
-#ifdef CONFIG_UCLAMP_TASK
 #ifdef CONFIG_UCLAMP_TASK_GROUP
 static void uclamp_update_root_tg(void)
 {
@@ -1840,7 +1839,6 @@ done:
 
 	return result;
 }
-#endif
 #endif
 
 static int uclamp_validate(struct task_struct *p,
@@ -2008,7 +2006,7 @@ static void __init init_uclamp(void)
 	}
 }
 
-#else /* CONFIG_UCLAMP_TASK */
+#else /* !CONFIG_UCLAMP_TASK */
 static inline void uclamp_rq_inc(struct rq *rq, struct task_struct *p) { }
 static inline void uclamp_rq_dec(struct rq *rq, struct task_struct *p) { }
 static inline int uclamp_validate(struct task_struct *p,
