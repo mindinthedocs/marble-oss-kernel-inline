f880ea67f88dd45b531afd9148f4ed56f10c1a03
mindinthedocs <testerfunny@hotmail.com>
2024-04-21 21:45:11 +0300

    Revert "ANDROID: sched: add vendor hook to PELT multiplier"
    This reverts commit 9e8dedef

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -394,10 +394,6 @@ DECLARE_HOOK(android_vh_setscheduler_uclamp,
 DECLARE_HOOK(android_vh_mmput,
 	TP_PROTO(void *unused),
 	TP_ARGS(unused));
-
-DECLARE_HOOK(android_vh_sched_pelt_multiplier,
-	TP_PROTO(unsigned int old, unsigned int cur, int *ret),
-	TP_ARGS(old, cur, ret));
 /* macro versions of hooks are no longer required */
 
 #endif /* _TRACE_HOOK_SCHED_H */
