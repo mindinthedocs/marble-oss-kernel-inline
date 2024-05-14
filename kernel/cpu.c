7865892bef6ccc8db31a53d5d84fd73d2eb1959d
mindinthedocs <testerfunny@hotmail.com>
2024-05-15 00:18:08 +0300

    Revert "ANDROID: cpuhp/pause: add trace points for pause and resume"
    This reverts commit 614afa949ce9bfa57876f3021a2ddc1abc576e4e.

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1160,9 +1160,6 @@ int pause_cpus(struct cpumask *cpus)
 {
 	int err = 0;
 	int cpu;
-	u64 start_time = 0;
-
-	start_time = sched_clock();
 
 	cpu_maps_update_begin();
 
@@ -1216,8 +1213,6 @@ err_cpus_write_unlock:
 err_cpu_maps_update:
 	cpu_maps_update_done();
 
-	trace_cpuhp_pause(cpus, start_time, 1);
-
 	return err;
 }
 EXPORT_SYMBOL_GPL(pause_cpus);
@@ -1226,9 +1221,6 @@ int resume_cpus(struct cpumask *cpus)
 {
 	unsigned int cpu;
 	int err = 0;
-	u64 start_time = 0;
-
-	start_time = sched_clock();
 
 	cpu_maps_update_begin();
 
@@ -1273,8 +1265,6 @@ err_cpus_write_unlock:
 	cpus_write_unlock();
 err_cpu_maps_update:
 	cpu_maps_update_done();
-
-	trace_cpuhp_pause(cpus, start_time, 0);
 
 	return err;
 }
