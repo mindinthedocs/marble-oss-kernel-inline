075f61e2967ad6137ba804e35fd44485a730fe6c
mindinthedocs <testerfunny@hotmail.com>
2024-05-15 01:01:46 +0300

    Revert "ANDROID: cgroup/cpuset: Fix suspicous RCU usage WARNING"
    This reverts commit ab6e10de9d51b681557a390fe272ec026d5c0cce.

--- kernel/cgroup/cpuset.c
+++ kernel/cgroup/cpuset.c
@@ -3416,17 +3416,14 @@ EXPORT_SYMBOL_GPL(cpuset_cpus_allowed);
 
 void cpuset_cpus_allowed_fallback(struct task_struct *tsk)
 {
+	const struct cpumask *cs_mask = task_cs(tsk)->cpus_allowed;
 	const struct cpumask *possible_mask = task_cpu_possible_mask(tsk);
-	const struct cpumask *cs_mask;
-
-	rcu_read_lock();
-	cs_mask = task_cs(tsk)->cpus_allowed;
 
 	if (!is_in_v2_mode() || !cpumask_subset(cs_mask, possible_mask))
-		goto unlock; /* select_fallback_rq will try harder */
+		return; /* select_fallback_rq will try harder */
 
+	rcu_read_lock();
 	do_set_cpus_allowed(tsk, cs_mask);
-unlock:
 	rcu_read_unlock();
 
 	/*
